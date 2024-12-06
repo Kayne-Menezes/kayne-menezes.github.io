68b6495cff9b739e2905daf40e460a72ce1c3aa2
K9Squad <kayne.menezes7@gmail.com>
2024-12-07 10:59:10 +1300

    fixed merging changes

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React from 'react'
 import TaskList from './TasksList'
-=======
-import React, { useState, useEffect } from 'react'
-import Tasks from './TodoList'
->>>>>>> 90b7c625ad6b65378b2cde7bc926cf083acdb12b
 
 function App() {
   return (
@@ -13,13 +8,7 @@ function App() {
         <h1>To-Do List</h1>
       </header>
       <section className="main">
-<<<<<<< HEAD
         <TaskList />
-=======
-        <div>
-          <Tasks />
-        </div>
->>>>>>> 90b7c625ad6b65378b2cde7bc926cf083acdb12b
       </section>
     </>
   )
